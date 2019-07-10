12c68ab3db65a2b4185fcef860c16a5ba31ed067
falcon78_infodex <falconhawk@protonmail.com>
2019-07-10 14:30:07 +0900

    added cors

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
-app.get('/searchPublished/:searchString', (req: any, res: any) => {
+app.use(cors())
+
+app.get('/searchPublished/:searchString', (req: any, res: any,next: any) => {
     console.log(req.body.title);
   let regex = new RegExp(req.params.searchString, 'gim');
   admin
@@ -18,7 +21,12 @@ app.get('/searchPublished/:searchString', (req: any, res: any) => {
       data.forEach((doc: any) => {
         const docData = doc.data();
         if (docData.title.match(regex)) {
-          matchDocuments.push(docData);
+          matchDocuments.push({
+              title : docData.title,
+              location: docData.location,
+              cardLocation: docData.cardLocation,
+              image: docData.image
+          });
         }
       });
       return res.json(matchDocuments);
